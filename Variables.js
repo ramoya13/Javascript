0aa9158b73a5a1d80123e1f1d5f02ad1c7250d11
ramoya <stevramoya@gmail.com>
2022-10-24 18:37:48 +0300

    Update Variables.js

--- Variables.js
+++ Variables.js
@@ -1,12 +1,32 @@
 // Example use of var to declare variable
 
-var myName;
+var myName; //declare variable
 
 // Storing values using the assignment operator
 
-myVar = 5;
+myVar = 5; // assigning variable
 
 // example
 
+var a; //declare variable
+a = 7; //assigning variable
+
+// Assigning value of a variable to another varable
+
+var myVar;
+myVar = 5;
+var myNum;
+myNum = myVar;
+
+// example
 var a;
-a =7;
+a = 7;
+var b;
+b = a;
+
+//Initializing variables with the assignment operator
+
+var myVar = 0; // Creates a new variable my var and initialisez it to 0
+
+//example
+var a = 9; // Creates a new variable a and initializes it to 9
